3782dc9d64230b2a937ac7ac8b8983f7bd17ee96
Nicholas D. Socci <soccin@mskcc.org>
2019-09-02 16:39:50 -0400

    Added notes from ENCODE

--- callPeaks_ChIPseq.sh
+++ callPeaks_ChIPseq.sh
@@ -5,40 +5,64 @@ MACS=/opt/common/CentOS_6/MACS2/MACS2-2.1.1/bin/macs2
 
 SDIR="$( cd "$( dirname "$0" )" && pwd )"
 
-IBED=$1
+GBUILD=$1
 
-if [ "$#" == "1" ]; then
-    PREFIX=${IBED/.bed}
-    echo $PREFIX
+case $GBUILD in
+    mm10)
+    MACS_GENOME=mm
+    ;;
+
+    b37)
+    MACS_GENOME=hs
+    ;;
+
+    *)
+    echo "Invalid Genome [$GBUILD]"
+    exit -1
+    ;;
+esac
+
+echo "MACS GENEOME =" $MACS_GENOME
+
+TBED=$2
+if [ "$#" == "3" ]; then
+    CBED=$3
 else
-    PREFIX=$2
+    CBED="_NoCTRL"
 fi
 
-ODIR=callpeaks/$PREFIX
+ODIR=$(dirname $TBED)/macs
 mkdir -p $ODIR
 
-TDIR=/scratch/socci/_scratch_ATACSeq/$(uuidgen -t)
-mkdir -p $TDIR
-echo $TDIR, $ODIR
-
-export TMPDIR=$TDIR
-
-#
-# Clean up BED file to remove non-main chromosomes
-#
-
-zcat $IBED \
-    | egrep -v "chrUn|_random" \
-    | gzip -c - > $TDIR/cleanBED.bed.gz
-
-# MACS2 args
-genome=mm
+# TDIR=/scratch/socci/_scratch_ATACSeq/$(uuidgen -t)
+# mkdir -p $TDIR
+# echo $TDIR, $ODIR
+# export TMPDIR=$TDIR
 
 #
 # From:
-#   https://github.com/kundajelab/atac_dnase_pipelines/blob/803e3d92435f52ef31b02fa264fecb5706d550e3/atac.bds#L16
-#   Line: 16
+#   https://github.com/ENCODE-DCC/chip-seq-pipeline/blob/master/dnanexus/macs2/src/macs2.py
 #
+
+    # # ===========================================
+    # # Generate narrow peaks and preliminary signal tracks
+    # # ============================================
+
+    # command = 'macs2 callpeak ' + \
+    #           '-t %s -c %s ' % (experiment.name, control.name) + \
+    #           '-f BED -n %s/%s ' % (peaks_dirname, prefix) + \
+    #           '-g %s -p 1e-2 --nomodel --shift 0 --extsize %s --keep-dup all -B --SPMR' % (genomesize, fraglen)
+
+
+    # # ===========================================
+    # # Generate Broad and Gapped Peaks
+    # # ============================================
+
+    # command = 'macs2 callpeak ' + \
+    #           '-t %s -c %s ' % (experiment.name, control.name) + \
+    #           '-f BED -n %s/%s ' % (peaks_dirname, prefix) + \
+    #           '-g %s -p 1e-2 --broad --nomodel --shift 0 --extsize %s --keep-dup all' % (genomesize, fraglen)
+
 
 smooth_window=150
 
