e3e37c4253d4f2bf88beae209836b4802a04e976
Nicholas D. Socci <soccin@mskcc.org>
2021-01-25 12:21:14 -0500

    Missing RSYNC for annotate res

--- deliverResults.sh
+++ deliverResults.sh
@@ -2,12 +2,21 @@
 
 RESDIR=$1
 
+if [ "$RESDIR" == "" ]; then
+    echo
+    echo "  usage: deliverResults.sh RESDIR"
+    echo "    RESDIR = .../pi/invest/projNo/r_###"
+    echo
+    exit
+fi
+
 mkdir -p $RESDIR/chipSeq/macs
 mkdir -p $RESDIR/chipSeq/bw
 
 if [ -e "./annote" ]; then
     echo "Annotation exists"
     mkdir -p $RESDIR/chipSeq/annote
+    rsync -rvP annote $RESDIR/chipSeq
 fi
 
 rsync -rvP out/profiles/*.bw $RESDIR/chipSeq/bw
