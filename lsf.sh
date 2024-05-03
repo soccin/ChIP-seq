109281e998c467d6a99fb44f9b92955dc0e28975
Nicholas D. Socci <soccin@mskcc.org>
2024-05-02 20:19:48 -0400

    Fix lsf/bsub wrapper

--- lsf.sh
+++ lsf.sh
@@ -1,7 +1,7 @@
-LSFBIN=/admin/lsflilac/lsf/10.1/linux3.10-glibc2.17-x86_64/bin/
+LSFBIN=/admin/lsfjuno/lsf/10.1/linux3.10-glibc2.17-x86_64/bin
 bsub () {
 
-    echo bsub -R "select[hname!=lt06]" $*
-    $LSFBIN/bsub -R "select[hname!=lt06]" $*
+    echo bsub $*
+    $LSFBIN/bsub $*
 
 }
